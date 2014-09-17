8a163aedb8cca805e0ade5654a7a79e6b2854152
kverrna <kverrna@gmail.com>
2014-09-17 12:08:52 -0300

    Looping no grafo implementado

--- grafo.hs
+++ grafo.hs
@@ -55,7 +55,10 @@ type Grafo =[Aresta]
 
 grafo ::[Aresta]
 grafo= [([(1,2,"(1) Carro"),(1,3,"(2) Jumento"),(1,4,"(3) Bus Lotado")],1,"Voce mora no goias e precisa ir para FGA, de que vc vai?"),
-		([(2,6,"(1) Colocar uma corrente envolta do carro"),(2,6,"(2) Levar um cachorro e amarrar ele na porta do carro")],2,"Estao Roubando Estepe na FGA, o que voce vai fazer pra se proteger?")]
+		([(2,6,"(1) Colocar uma corrente envolta do carro"),(2,6,"(2) Levar um cachorro e amarrar ele na porta do carro")],2,"Estao Roubando Estepe na FGA, o que voce vai fazer pra se proteger?"),
+		 ([(3,1,"(1) Desisto, vou a pe"),(3,1,"(2) Nao vou a aula hoje"),(3,4,"(3) Desito, vou de bus")],3,"O Jumento impacou, e agora?"),
+		 ([(6,2,"(1) OP 1"),(6,1,"(2) Op 2"),(6,4,"(3)Op 3")],6,"Deu certo")
+		]
 
 selec_prim (x,_,_)=x
 selec_sec (_,x,_)=x
--- main.hs
+++ main.hs
@@ -2,27 +2,75 @@ module Main where
 
 import Grafo (grafo,retorna_aresta,filtra_questoes_aresta,ver_questoes_aresta,ver_informacao_aresta,selec_sec)
 
---imprimeQuestoes ::[String]
---imprimeQuestoes (a:xs)= putStrLn a
+--startGame:: Int-> IO()
+--startGame idStart = do
+	
+--	putStrLn ""
+--	opcao <-readLn
+--	print(ver_informacao_aresta (retorna_aresta grafo (selec_sec(filtra_questoes_aresta(retorna_aresta grafo idStart)!!(opcao-1)))))
+--	putStrLn ""
+--	--(selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))   --retorna Id da proxima aresta
+--	--idProx<-(selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))
+	
+--	putStrLn "Proximo No"
+--	putStrLn "-------"
+--	print (selec_sec(filtra_questoes_aresta(retorna_aresta grafo idStart)!!(opcao-1)))
+--	putStrLn "-------"
+	
+--	print(ver_questoes_aresta(filtra_questoes_aresta(retorna_aresta grafo (selec_sec(filtra_questoes_aresta(retorna_aresta grafo idStart)!!(opcao-1))))))	
+--	putStrLn ""
+--	let idStart=selec_sec(filtra_questoes_aresta(retorna_aresta grafo idStart)!!(opcao-1))
+
+--	putStrLn "O que vc vai fazer? (digite um dos numeros)"
+--	putStrLn ""
+
+--	if (opcao<100)then startGame idStart
+--		else putStrLn "Stop "
+
+startGame_2::Int -> IO()
+startGame_2 idStart = do
+	
+	print(ver_informacao_aresta (retorna_aresta grafo idStart))
+	putStrLn ""
+	print(ver_questoes_aresta(filtra_questoes_aresta(retorna_aresta grafo idStart)))
+	putStrLn "O que vc vai fazer? (digite um dos numeros)"
+	opcao <-readLn
+	let aux=selec_sec(filtra_questoes_aresta(retorna_aresta grafo idStart)!!(opcao-1))
+	let idStart=aux
+
+	if (opcao<100)then startGame_2 idStart
+		else putStrLn "Stop "
+
 
 main =do
 	putStrLn "Olá, vamos iniciar o Jogo!"
+	putStrLn ""
 	putStrLn "Qual eh o seu nome?"
+	putStrLn ""
 	nome <- getLine
 	putStrLn ("Ok "++nome++" Seja bem vindo(a)")
 	
-	print(ver_informacao_aresta (retorna_aresta grafo 1))
-	putStrLn ""
-	putStrLn $nome++" o que vc faz? (digite um dos numeros)"
-	putStrLn ""
-	print(ver_questoes_aresta(filtra_questoes_aresta(retorna_aresta grafo 1)))
-	putStrLn ""
-	opcao <-readLn
-	print(ver_informacao_aresta (retorna_aresta grafo (selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))))
-	putStrLn ""
-	--(selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))   --retorna Id da proxima aresta
-	--idProx<-(selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))
-	print(ver_questoes_aresta(filtra_questoes_aresta(retorna_aresta grafo (selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1))))))	
-	putStrLn ""
+	
+	startGame_2 1
+	
+	--print(ver_informacao_aresta (retorna_aresta grafo 2))
+	--putStrLn ""
+
+	--print(ver_questoes_aresta(filtra_questoes_aresta(retorna_aresta grafo 2)))
+	--putStrLn "O que vc vai fazer? (digite um dos numeros)"
+	--startGame 1
+
+
+	--putStrLn ""
+	--putStrLn $nome++" o que vc vai fazer? (digite um dos numeros)"
+	--putStrLn ""
+	
+	--opcao <-readLn
+	--print(ver_informacao_aresta (retorna_aresta grafo (selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))))
+	--putStrLn ""
+	----(selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))   --retorna Id da proxima aresta
+	----idProx<-(selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1)))
+	--print(ver_questoes_aresta(filtra_questoes_aresta(retorna_aresta grafo (selec_sec(filtra_questoes_aresta(retorna_aresta grafo 1)!!(opcao-1))))))	
+	--putStrLn ""
 	
 
