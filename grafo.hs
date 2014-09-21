0d0d96d1f5af99ec02e7366d5ceac9aa16111af7
kverrna <kverrna@gmail.com>
2014-09-21 06:27:23 -0700

    Santa maria grafo

--- grafo.hs
+++ grafo.hs
@@ -25,6 +25,20 @@ grafo = [
 		([(15,11,"(1) Moto Taxi"), (15,11,"(2) Esperar")], 15, "Mais uma mare de sorte. Houve um acidente, esta tudo parado, o que voce vai fazer?")
 		]
 
+--grafo = [
+--			([(1,2,"(1) De Onibus."),(1,3,"(2) De Carro."),(1,4,"(3) De Bike."),(1,5,"(4) De Carroca.")],1,"Voce mora em Santa Maria, como vc vai para FGA?"),
+--			([(2,6,"(1) Pegar um Taxi."),(2,7,"(2) Fazer respiracao boca a boca na vaca e salvar ela."),(2,4,"(3) Voltar para casa e pegar a Bike.")],2,"O motorista atropelou uma vaca sagrada e esta sendo linxado por indianos."),
+--			([(3,6,"(1) Abandonar o carro e pegar um taxi."),(3,9,"(2) Abandonar o carro e ir a pe mesmo.")],3,"O pneu furou e voce esta sem Estepe."),
+--			([(4,5,"(1) Pegar uma carona numa carroca."),(4,8,"(2) Roubar a bicicleta de uma crianca.")],4,"Voce foi assaldato por um bandido cego."),
+--			([(5,10,"(1) Puxar a carroca no braco."),(5,9,"(2) Abandonar e ir a pe para FGA.")],5,"O cavalo se soltou e correu atras de uma egua "),
+--			([(6,11,"(1) Sequestrar o taxi e ir para FGA. "),(6,9,"(2) Pular do carro em movimento e ir andando.")],6,"Voce esta na metade do caminho e viu que esta sem dinheiro."),
+--			([(7,11,"(1) Correr dos indianos e vai a pe para FGA."),(7,9,"(2) Ordenar que eles o carreguem ate a FGA.")],7,"Os indianos estao te adorando pensando que voce eh o deus shiva. "),
+--			([(8,9,"(1) Correr bem rapido e vai a pe para FGA."),(8,2,"(2) Correr bem rapido e entra no primeiro onibus que voce ver.")],8,"O pai da crianca eh um negao bombado de 2 metros de altura e esta muito bravo. "),
+--			([(9,11,"(1) Amarrar sua toca no pe e vai andando."),(9,11,"(2) Ir pulando de um pe so")],9,"A sola do seu sapato soltou e o asfalto esta muito quente. "),	
+--			([(10,9,"(1) Abandonar e ir a pe para FGA."),(10,8,"(2) Roubar a bicicleta de uma crianca e ir para FGA.")],10,"A carroca desmontou. "),
+--			([(11,1,"Digite 1 para voltar para Casa")],11,"A FGA esta fechada, hoje eh domingo, voce confundiu os dias =(")
+
+--		]
 selec_prim (x,_,_) = x
 selec_sec (_,x,_) = x
 selec_ter (_,_,x) = x
